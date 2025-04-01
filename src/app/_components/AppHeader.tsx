38d6f2b06d72b95d2c25da8c0e4e5acf307f1343
TimeCyber <229637012@qq.com>
2025-04-01 16:24:59 +0800

    Initial commit

---
+++ src/app/_components/AppHeader.tsx
@@ -0,0 +1,13 @@
+export function AppHeader() {
+  return (
+    <div>
+      <a
+        className="font-serif text-lg font-extralight text-gray-500"
+        href="https://github.com/DeepManus/DeepManus"
+        target="_blank"
+      >
+        DeepManus
+      </a>
+    </div>
+  );
+}
